0c40c5531f5b2e79819d1fcdeff017749c892342
victorpb96 <120343714+victorpb96@users.noreply.github.com>
2024-03-22 13:50:26 +0800

    trying to get racehorse img to load on the body

---
+++ src/app/Header/Header.js
@@ -0,0 +1,11 @@
+import racehorse from '/public/racehorse.jpg'
+
+export default function Header() {
+    return(
+        <>
+        <div>
+            <img src="/pages/racehorse.jpg" alt="racehorse" width={300}></img>
+        </div>
+        </>
+    );
+}
