aedf1c6290b163df81272ac6602fce715e599c83
victorpb96 <120343714+victorpb96@users.noreply.github.com>
2024-04-04 22:52:04 +0800

    simple yet works

---
+++ src/app/Toptips/Top-tips.jsx
@@ -0,0 +1,10 @@
+import StyleSheet from './Top-tips.css'
+import Image from 'next/image';
+
+export default function Toptips() {
+    return (
+        <div className="Top-tips-grid">
+            <div>1 - Racehorse</div>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,21 @@
-import Image from "next/image";
+import Image from 'next/image';
 import Link from 'next/link';
 import Header from './Header/Header.jsx';
+import Toptips from './Toptips/Top-tips.jsx';
 
 export default function Home() {
   return (
-    <>
+    <div>
       <div class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
           <Link href="/">The Bang Team</Link>
             <div class="flex items-center space-x-3 float-right">
-                <Link href="/">About</Link>
             </div>
           <Link href="/">Contact</Link>
         </div>
       </div>
       <Header />
-    </>
+      <Toptips />
+    </div>
   );
 }
