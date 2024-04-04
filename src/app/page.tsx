74b06a90507b1bafda7cc9e87f6515034aa4c1fa
victorpb96 <120343714+victorpb96@users.noreply.github.com>
2024-04-04 22:58:19 +0800

    className - added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import Toptips from './Toptips/Top-tips.jsx';
 export default function Home() {
   return (
     <div>
-      <div class="bg-white border-gray-200 dark:bg-gray-900">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
+      <div className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
           <Link href="/">The Bang Team</Link>
-            <div class="flex items-center space-x-3 float-right">
+            <div className="flex items-center space-x-3 float-right">
             </div>
           <Link href="/">Contact</Link>
         </div>
